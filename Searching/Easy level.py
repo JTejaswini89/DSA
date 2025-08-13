c3c43c420d26a018a1fa584f7a7feb7386da97fe
Tejeswini <129767798+JTejaswini89@users.noreply.github.com>
2025-08-13 13:33:35 +0530

    Update Easy level.py

--- Searching/Easy level.py
+++ Searching/Easy level.py
@@ -30,7 +30,7 @@ print(sort(arr,n))
 """Second Largest Element in an Array
 Given an array of positive integers arr[] of size n, the task is to find
 second largest distinct element in the array.
-Note: If the second largest element does not exist, return -1.
+Note: If the elements does not exist, return -1.
 
 Examples:
 
@@ -225,3 +225,4 @@ print(' '.join(map(str, res)))
 
 
     
+
